bce204fe93a6193601af09ddca1e17e2e91a692a
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2025-01-11 11:41:35 +0600

    Update 01. Find Even and Odd.cpp

--- 01. Find Even and Odd.cpp
+++ 01. Find Even and Odd.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int T,i,n;
+    int T,n;
     cin>>T;
-    for(i=1;i<=T;i++ ){
+    for(int i=1;i<=T;i++ ){
         cin>>n;
         if(n%2==0){
-            cout<<"Even";
+            cout<<"Even"<<endl;
         }
         else{
-            cout<<"Odd";
+            cout<<"Odd" <<endl;
         }
     }
     return 0;
